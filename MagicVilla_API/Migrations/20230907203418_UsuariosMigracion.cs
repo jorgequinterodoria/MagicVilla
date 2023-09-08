8445a9de27bc18b45714eefb8ad5f54c9ee2631c
Jorge Quintero <jquintedori@gmail.com>
2023-09-08 09:31:41 -0500

    Creacion Usuarion

---
+++ MagicVilla_API/Migrations/20230907203418_UsuariosMigracion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsuariosMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 9, 7, 15, 34, 18, 272, DateTimeKind.Local).AddTicks(6309), new DateTime(2023, 9, 7, 15, 34, 18, 272, DateTimeKind.Local).AddTicks(6290) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 9, 7, 15, 34, 18, 272, DateTimeKind.Local).AddTicks(6316), new DateTime(2023, 9, 7, 15, 34, 18, 272, DateTimeKind.Local).AddTicks(6315) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 9, 6, 8, 52, 50, 613, DateTimeKind.Local).AddTicks(1769), new DateTime(2023, 9, 6, 8, 52, 50, 613, DateTimeKind.Local).AddTicks(1751) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 9, 6, 8, 52, 50, 613, DateTimeKind.Local).AddTicks(1781), new DateTime(2023, 9, 6, 8, 52, 50, 613, DateTimeKind.Local).AddTicks(1779) });
+        }
+    }
+}
--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,6 +1,7 @@
 using MagicVilla_API;
 using MagicVilla_API.Datos;
 using MagicVilla_API.Repositorio;
+using MagicVilla_API.Repositorio.IRepositorio;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddScoped<IVillaRepositorio, VillaRepositorio>();
 builder.Services.AddScoped<INumeroVillaRepositorio, NumeroVillaRepositorio>();
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 
 var app = builder.Build();
 
